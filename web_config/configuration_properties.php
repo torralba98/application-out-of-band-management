16643bcecfe1cccb260dda9ed7a8b33e6a1d390f
torralba98 <78704501+torralba98@users.noreply.github.com>
2021-02-17 22:07:53 +0100

    Update configuration_properties.php

--- web_config/configuration_properties.php
+++ web_config/configuration_properties.php
@@ -4,7 +4,7 @@
 	$dbuser	= "<USERNAME>";		  // database username
 	$dbpass	= "<USERNAME_PASSWORD>";	  // database password
 	$dbname	= "<DB_NAME>";    		  // database name
-	$web_url = "<WEB_URL>"; 	  // Web URL
+	$web_url = "<URL>"; 	  // Web URL
 	$emailPHP = "<E-MAIL>";  	  // e-mail used by PHPMailer
 	$emailPHPpass = "<E-MAIL_PASS>"; // e-mail password
 ?>
