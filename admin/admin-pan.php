018eccb1a09b106375368b819f43a269dcabf22a
torralba98 <78704501+torralba98@users.noreply.github.com>
2021-02-19 23:16:39 +0100

    Update admin-pan.php

--- admin/admin-pan.php
+++ admin/admin-pan.php
@@ -42,19 +42,19 @@
 <!doctype html>
 <html lang="es">
 
-	<head>
+   <head>
 
-  	<title>Panel de Administración</title>
-  	<link rel="icon" type="image/png" href="/images/icon.png" />
-  	<?php include "$root/web/header.php"; ?>
+     <title>Panel de Administración</title>
+     <link rel="icon" type="image/png" href="/images/icon.png" />
+     <?php include "$root/web/header.php"; ?>
 
-  	<!-- Required meta tags -->
-  	<meta charset="utf-8">
-  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  	<!-- CSS -->
-  	<link rel="stylesheet" href="../admin/css/admin.css">
-  	<link rel="stylesheet" href="../css/bootstrap.min.css">
+     <!-- CSS -->
+     <link rel="stylesheet" href="../admin/css/admin.css">
+     <link rel="stylesheet" href="../css/bootstrap.min.css">
 
      <script>
 
@@ -64,22 +64,22 @@
 
     </script>
   	
-	</head>
+  </head>
 
-	<body background="/images/background.jpg">
+  <body background="/images/background.jpg">
 
     <br>
     <nav class='menuHK container'><ul>
-    <li><a href="registered-users">Usuarios</a>|</li>
-    <li><a href="devices">Dispositivos</a>|</li>
-    <li><a href="logs">Logs</a>|</li>
-    <li><a href="assignments">Asignaciones Users/Devices</a>|</li>
-    <li><a href="reset-devices">Resetear Dispositivos</a></li>
+	    <li><a href="registered-users">Usuarios</a>|</li>
+	    <li><a href="devices">Dispositivos</a>|</li>
+	    <li><a href="logs">Logs</a>|</li>
+	    <li><a href="assignments">Asignaciones Users/Devices</a>|</li>
+	    <li><a href="reset-devices">Resetear Dispositivos</a></li>
     </ul></nav>
 
-		<div class="container">
+    <div class="container">
 
-  		<div class='alert alert-success mt-4' role='alert'>
+     <div class='alert alert-success mt-4' role='alert'>
         <div id="content1">
           <img src="/images/admin_pan.png" style="width:150px;height:150px; position: absolute; top: 0; right: 0; margin-right: 25px; margin-top: 18px;" />
         </div>
@@ -88,10 +88,10 @@
               <FONT SIZE=3><i><p><a>Aquí podrás <b>administrar</b> información relativa a los diferentes <b>grupos de usuarios</b>, <b>grupos de dispositivos</b> y <b>logs</b>.</a></p></i></font>
               <br>
         </div>
-		</div>
+      </div>
 
-	</body>
+  </body>
 
-  <?php include "$root/web/footer.php"; ?>
+ <?php include "$root/web/footer.php"; ?>
   
 </html>
